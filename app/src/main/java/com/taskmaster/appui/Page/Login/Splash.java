191f31c9778642eca642f8cf07af052cb6d634ea
Centhor <vincenthizon266@gmail.com>
2025-06-10 23:17:56 +0800

    splash checks if user exists (logged in before), moves to QuestManagement instead of login if true

--- app/src/main/java/com/taskmaster/appui/Page/Login/Splash.java
+++ app/src/main/java/com/taskmaster/appui/Page/Login/Splash.java
@@ -1,6 +1,5 @@
 package com.taskmaster.appui.Page.Login;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -9,45 +8,32 @@ import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.taskmaster.appui.Page.Main.QuestManagement;
 import com.taskmaster.appui.Services.NavUtil;
 import com.taskmaster.appui.R;
 
 public class Splash extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 3000;
-
     // var
     Animation pop_out_Anim, fade_in_Anim;
     ImageView bg, logo, logo_shadow;
 
-    Intent LoginIntent;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.splash);
 
-        // init intent
-        LoginIntent = new Intent(Splash.this, UserLogin.class);
-
         // hide status bar and nav bar
         NavUtil.hideSystemBars(this);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         // anim
         pop_out_Anim = AnimationUtils.loadAnimation(this, R.anim.pop_out_animation);
         fade_in_Anim = AnimationUtils.loadAnimation(this, R.anim.fade_in_animation);
-
 
         // hooks
         bg = findViewById(R.id.imageView4);
@@ -59,13 +45,13 @@ public class Splash extends AppCompatActivity {
         logo.setAnimation(pop_out_Anim);
         logo_shadow.setAnimation(pop_out_Anim);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-                startActivity(LoginIntent);
-                finish();
+        new Handler().postDelayed(() -> {
+            if(user!=null){
+                NavUtil.instantNavigation(Splash.this,QuestManagement.class);
             }
-        }, SPLASH_SCREEN);
+            else{
+                NavUtil.instantNavigation(Splash.this,UserLogin.class);
+            }
+        }, 3000);
     }
 }
