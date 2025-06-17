3b840361e37b4435a79151d7d5f0619176d089e4
Centhor <vincenthizon266@gmail.com>
2025-06-17 17:20:20 +0800

    Do not ask how I coded this. CosmeticShop module, database implementation not added

--- app/src/main/java/com/taskmaster/appui/Page/CosmeticShop.java
+++ app/src/main/java/com/taskmaster/appui/Page/CosmeticShop.java
@@ -1,8 +1,18 @@
 package com.taskmaster.appui.Page;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.taskmaster.appui.R;
+import com.taskmaster.appui.Services.CosmeticItemTemplate.CosmeticAdapter;
+import com.taskmaster.appui.Services.CosmeticItemTemplate.CosmeticItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CosmeticShop extends AppCompatActivity {
 
@@ -11,8 +21,27 @@ public class CosmeticShop extends AppCompatActivity {
         //Setup
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cosmetic_shop);
+        RecyclerView recycler = findViewById(R.id.recyclerView);
+        List<CosmeticItem> data = new ArrayList<>();
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar1));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar2));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar3));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar4));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar5));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar1));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar2));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar3));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar4));
+        data.add(new CosmeticItem("Silver Armor", "High-level protection", 2, R.drawable.placeholderavatar5));
 
+        CosmeticAdapter adapter = new CosmeticAdapter(data, pos -> {
+            // handle click
+            CosmeticItem clicked = data.get(pos);
+            Toast.makeText(this, String.valueOf(clicked.price), Toast.LENGTH_SHORT).show();
+        });
 
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(adapter);
     }
 
 }
