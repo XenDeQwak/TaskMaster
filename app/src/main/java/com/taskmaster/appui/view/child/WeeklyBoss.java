6eefcb1106772d8c79e1d89bc12059c4a168b2dd
Centhor <vincenthizon266@gmail.com>
2025-06-28 19:18:49 +0800

    WeeklyBoss all functionalities working (ProgressionPage Not Working)

--- app/src/main/java/com/taskmaster/appui/view/child/WeeklyBoss.java
+++ app/src/main/java/com/taskmaster/appui/view/child/WeeklyBoss.java
@@ -7,82 +7,66 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.constraintlayout.widget.Group;
 
-import com.taskmaster.appui.entity.User;
+import com.google.firebase.firestore.FieldValue;
+import com.taskmaster.appui.entity.Boss;
 import com.taskmaster.appui.util.*;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.taskmaster.appui.Page.Main.User;
+import com.taskmaster.appui.entity.User;
 import com.taskmaster.appui.R;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class WeeklyBoss extends AppCompatActivity {
     AppCompatButton fightButton, statReqStr, statReqInt, monsterName, childBarStatsButton, popupMonsterButton, childBarFloorCount;
-    ImageView statGraph, childAvatarImage, popupMonsterImage;
-    TextView popupMonsterMessageText, monsterHealthBarText, popupMonsterName;
+    ImageView popupMonsterImage;
+    TextView popupMonsterMessageText, monsterHealthBarText;
     Group dropDownGroup, popupMonsterMessage;
     ProgressBar monsterHealthBar;
     View rootLayout;
     TextView timerTxtDays;
-    int bossReq = 10;
-    int childStr;
-    int childInt;
-    int bossAvatar;
-    String bossName;
     private final Handler handler = new Handler(Looper.getMainLooper());
-    private int currentProgress = 100;
-    private List<Integer> avatarImages;
-    private List<String> avatarNames;
-    private int currentImageIndex = 0;
-    int childAvatar;
-    DocumentReference bossDoc;
-    FirebaseFirestore db;
+
     TextView childBarName;
     Group childBarGroup;
     ImageView childBarAvatar;
-    String bossID;
-    int floorCount;
+    DocumentSnapshot childDocument;
+    User user;
+
     ImageView monsterImage;
-    FirebaseAuth auth;
-    private boolean bossIsDead;
+
     TextView timerTxt;
+
+    private Boss currBoss;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.weekly_boss);
         // hide status bar and nav bar
         NavUtil.hideSystemBars(this);
 
         // hooks
         {
-            timerTxtDays=findViewById(R.id.TimerTxtDays);
+            timerTxtDays = findViewById(R.id.TimerTxtDays);
+            timerTxt = findViewById(R.id.TimerTxt);
             dropDownGroup = findViewById(R.id.dropdownGroup);
             childBarStatsButton = findViewById(R.id.childBarStatsButton);
             rootLayout = findViewById(R.id.statContainer);
             childBarName = findViewById(R.id.childBarName);
             childBarGroup = findViewById(R.id.childBarGroup);
             childBarAvatar = findViewById(R.id.childBarAvatar);
-            popupMonsterName = findViewById(R.id.popupMonsterName);
-
             fightButton = findViewById(R.id.fightButton);
             popupMonsterButton = findViewById(R.id.popupMonsterButton);
             popupMonsterMessage = findViewById(R.id.popupMonsterMessage);
             popupMonsterMessageText = findViewById(R.id.popupMonsterMessageText);
+            popupMonsterImage = findViewById(R.id.popupMonsterImage);
             monsterHealthBar = findViewById(R.id.monsterHealthBar);
             monsterHealthBarText = findViewById(R.id.monsterHealthBarText);
             childBarFloorCount = findViewById(R.id.childBarFloorCount);
@@ -90,128 +74,31 @@ public class WeeklyBoss extends AppCompatActivity {
             statReqStr = findViewById(R.id.statReqStr);
             statReqInt = findViewById(R.id.statReqInt);
             monsterImage = findViewById(R.id.monsterImage);
-            popupMonsterImage = findViewById(R.id.popupMonsterImage);
-
             childBarFloorCount = findViewById(R.id.childBarFloorCount);
-            // ... inside onCreate() after initializing views:
             fightButton = findViewById(R.id.fightButton);
-            timerTxt = findViewById(R.id.TimerTxt);
         }
         DropdownUtil.dropdownSetup(this, rootLayout);
         NavUtil.setNavigation(this, childBarStatsButton, ProgressionPage.class);
+        user = User.getInstance();
+        childDocument = user.getDocumentSnapshot();
         TimeUtil timeUtil = TimeUtil.getInstance();
-        timeUtil.setupTimer();
-        timeUtil.startTimer(new TimeUtil.TimerListener() {
-            @Override
-            public void onTick(long timeRemaining) {
-                setTime(timeRemaining);
-            }
+        if(childDocument.getDouble("bossTimer") == 0){
+            timeUtil.setupTimer(callback -> startTimer());
+        }else{
+            startTimer();
+        }
 
-        // hide popup
-        popupMonsterMessage.setVisibility(View.GONE);
-
-
-        //child data init
-        db = FirebaseFirestore.getInstance();
-        auth = FirebaseAuth.getInstance();
-
-        Map<String, Object> bossData = new HashMap<>();
-
-//        db.collection("users").document(userId).get()
-//                .addOnCompleteListener(task -> {
-//                    if (task.isSuccessful()) {
-//                        DocumentSnapshot childDocument = task.getResult();
-//                        if (childDocument.exists()) {
-//                            db.collection("boss").get()
-//                                    .addOnCompleteListener(tasks -> {
-//                                        if (tasks.isSuccessful()) {
-//                                            if (tasks.getResult().isEmpty()) {
-//                                                bossData.put("bossReq", bossReq);
-//                                                bossData.put("bossAvatar", 0);
-//                                                bossData.put("bossIsDead", false);
-//                                                bossID = db.collection("boss").document().getId();
-//                                                db.collection("boss").document(bossID).set(bossData);
-//                                            } else {
-//                                                QuerySnapshot bossDocs = tasks.getResult();
-//                                                bossID = bossDocs.getDocuments().get(0).getId();
-//                                                bossDoc = db.collection("boss").document(bossID);
-//                                                bossAvatar = bossDocs.getDocuments().get(0).getLong("bossAvatar").intValue();
-//                                                bossIsDead = bossDocs.getDocuments().get(0).getBoolean("bossIsDead");
-//                                                List<Integer>bossImages = new ArrayList<>();
-//                                                bossImages.add(R.drawable.bucklerbossundamaged);
-//                                                bossImages.add(R.drawable.bookbossundamaged);
-//
-//                                                if (bossAvatar == 0) {
-//                                                    monsterImage.setImageResource(bossImages.get(0));
-//                                                    monsterName.setText("SHIELD");
-//                                                    popupMonsterName.setText("SHIELD");
-//                                                    popupMonsterImage.setImageResource(R.drawable.bucklerbossdamaged);
-//                                                } else {
-//                                                    monsterImage.setImageResource(bossImages.get(1));
-//                                                    monsterName.setText("BOOK");
-//                                                    popupMonsterName.setText("BOOK");
-//                                                    popupMonsterImage.setImageResource(R.drawable.bookbossdamaged);
-//                                                }
-//
-//                                                bossReq = bossDocs.getDocuments().get(0).getLong("bossReq").intValue();
-//                                                statReqStr.setText("STR: " + bossReq);
-//                                                statReqInt.setText("INT: " + bossReq);
-//                                            }
-//                                        }
-//                                    });
-//
-//                            childStr = childDocument.getLong("childStr").intValue();
-//                            childInt = childDocument.getLong("childInt").intValue();
-//                            floorCount = childDocument.getLong("floor").intValue();
-//                            childAvatar = childDocument.getLong("childAvatar").intValue();
-//                            childBarGroup.setVisibility(View.VISIBLE);
-//                            childBarName.setText(childDocument.getString("username"));
-//                            childBarFloorCount.setText("Floor " + floorCount);
-//                            //startBossTimer();
-//
-//
-//                            List<Integer>avatarImages = new ArrayList<>();
-//                            avatarImages.add(R.drawable.placeholderavatar5_framed_round);
-//                            avatarImages.add(R.drawable.placeholderavatar1_framed_round);
-//                            avatarImages.add(R.drawable.placeholderavatar2_framed_round);
-//                            avatarImages.add(R.drawable.placeholderavatar3_framed_round);
-//                            avatarImages.add(R.drawable.placeholderavatar4_framed_round);
-//
-//                            childBarAvatar.setImageResource(avatarImages.get(childAvatar));
-//                            Log.d("FLOOR", "FLOOR: " + floorCount);
-//                            DocumentReference docRef = db.collection("users").document(userId);
-//                            fightButton.setOnClickListener(e -> {
-//                                bossFight(docRef, bossID, bossDoc);
-//                            });
-//                        } else {
-//                            Log.d("DEBUG", "PARENT DOCUMENT DOES NOT EXIST");
-//                        }
-//                    } else {
-//                        Log.d("DEBUG", "Error getting parent document", task.getException());
-//                    }
-//                });
-
-
-        updateProgressBar(currentProgress);
-        popupMonsterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                popupMonsterMessage.setVisibility(View.GONE);
-            }
-        });
+        setUpBoss();
+        setUpAvatar();
+        popupMonsterButton.setOnClickListener(v -> popupMonsterMessage.setVisibility(View.GONE));
     }
-    private void updateProgressBar(int progress) {
-        int clampedProgress = Math.max(0, Math.min(progress, 100));
-        monsterHealthBar.setProgress(clampedProgress);
-        monsterHealthBarText.setText(clampedProgress + "/100");
-    }
-    public void setTime(long ms){
+
+    public void setTime(long ms) {
 
         Long days = ms / 86400000;
         Long hours = (ms % 86400000) / 3600000;
         Long minutes = (ms % 3600000) / 60000;
         Long seconds = (ms % 60000) / 1000;
-
         String timeFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds);
         runOnUiThread(() -> {
             timerTxtDays.setText(days + " Days");
@@ -219,29 +106,144 @@ public class WeeklyBoss extends AppCompatActivity {
         });
     }
 
-    private void startTimer(){
+    private void startTimer() {
         TimeUtil timeUtil = TimeUtil.getInstance();
         timeUtil.startTimer(new TimeUtil.TimerListener() {
             @Override
             public void onTick(long timeRemaining) {
                 setTime(timeRemaining);
             }
-
             @Override
             public void onFinish() {
-                //if(bossDefeated){
-                //    bossDefeated = false; goodjob or something floor advance
-                //}else{
-                //    popupMonsterMessageText.setText("Time's up! You lose!");
-                //        childRef.update("childStr", FieldValue.increment(-5));
-                //        childRef.update("childInt", FieldValue.increment(-5));
-                //}
-                Toast.makeText(getApplicationContext(), "Time's up! You lose!", Toast.LENGTH_SHORT).show();
+                user.loadDocumentSnapshot(callback->{
+                    childDocument=user.getDocumentSnapshot();
+                    timeUtil.setupTimer(callback1 -> {
+                        if(childDocument.getBoolean("bossAlive")){ //Fail
+                            childDocument.getReference().update(
+                                "Strength", FieldValue.increment(-5),
+                                "Intelligence", FieldValue.increment(-5)
+                            ).addOnCompleteListener(e->{
+                                    attachRestartHandler();
+                                    showPopup("Time's up! You lose!","Aww");
+                            });
 
-                timeUtil.setupTimer(callback->{
-                    startTimer();
+                        }else{
+                            childDocument.getReference().update(
+                                "bossAlive", true,
+                                "Floor", FieldValue.increment(+1)
+                            ).addOnCompleteListener(e->{
+                                    attachRestartHandler();
+                                    showPopup("Congratulations, You Move Up a Floor", "Next Floor");
+                                });
+                        }
+                    });
                 });
             }
         });
+    }
+    private void attachRestartHandler() {
+        popupMonsterButton.setOnClickListener(v -> {
+            // one last fresh fetch to be extra safe
+            user.loadDocumentSnapshot(callback ->{
+                childDocument = user.getDocumentSnapshot();
+                NavUtil.instantNavigation(this,this.getClass());
+                overridePendingTransition(0,0);
+            });
+        });
+    }
+    private void setUpAvatar(){
+        List<Integer> avatarImages = new ArrayList<>();
+        avatarImages.add(R.drawable.placeholderavatar1_framed_round);
+        avatarImages.add(R.drawable.placeholderavatar2_framed_round);
+        avatarImages.add(R.drawable.placeholderavatar3_framed_round);
+        avatarImages.add(R.drawable.placeholderavatar4_framed_round);
+        avatarImages.add(R.drawable.placeholderavatar5_framed_round);
+        childBarName.setText(childDocument.getString("Username"));
+        childBarAvatar.setImageResource(avatarImages.get(childDocument.getDouble("Avatar").intValue()));
+    }
+
+    private void setUpBoss(){
+        int floor = childDocument.getDouble("Floor").intValue();
+        childBarFloorCount.setText("Floor " + floor);
+        Boss[] bosses = new Boss[]{
+                new Boss("Buckler", R.drawable.bucklerbossundamaged_sprite, R.drawable.bucklerbossdamaged_sprite),
+                new Boss("Book", R.drawable.bookbossundamaged_sprite, R.drawable.bookbossdamaged_sprite),
+                new Boss("Glove", R.drawable.gloveboss_sprite, R.drawable.glovebossdamaged_sprite),
+                new Boss("Spirit Animal", R.drawable.spiritanimalboss, R.drawable.spiritanimalbossdamaged),
+                new Boss("Witch Hat", R.drawable.witchhatboss_sprite, R.drawable.witchhatbossdamaged_sprite)
+        };
+        int index = (floor % bosses.length);
+        currBoss = bosses[index];
+        statReqStr.setText("STR: " + floor);
+        statReqInt.setText("INT: " + floor);
+        if(childDocument.getBoolean("bossAlive")){
+            updateProgressBar(0);
+            monsterImage.setImageResource(currBoss.getUndamagedImageResId());
+            int childStr = childDocument.getDouble("Strength").intValue();
+            int childInt = childDocument.getDouble("Intelligence").intValue();
+            fightButton.setOnClickListener(e -> bossFight(childStr, childInt, floor));
+        }else{
+            updateProgressBar(100);
+            monsterImage.setImageResource(currBoss.getDamagedImageResId());
+            fightButton.setAlpha(0.70f);
+            fightButton.setText("Defeated");
+            fightButton.setOnClickListener(e-> showPopup("Fight the Next Boss\nNext Week","Okay"));
+        }
+        monsterName.setText(currBoss.getName());
+    }
+
+    private void updateProgressBar(int progress) {
+        progress = 100-progress;
+        monsterHealthBar.setProgress(progress);
+        monsterHealthBarText.setText(progress + "/100");
+    }
+
+    private void showPopup(String message, String buttonText){
+        if(childDocument.getBoolean("bossAlive")){
+            popupMonsterImage.setImageResource(currBoss.getUndamagedImageResId());
+        }else{
+            popupMonsterImage.setImageResource(currBoss.getDamagedImageResId());
+        }
+        popupMonsterMessageText.setText(message);
+        popupMonsterButton.setText(buttonText);
+        popupMonsterMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void bossFight(int strength, int intelligence, int floor) {
+        int[] i = {5};
+        int[] damage={0};
+        if (strength >= floor && intelligence >= floor) {
+            Runnable damageRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    if (i[0]>0) {
+                        damage[0] +=20;
+                        i[0]--;
+                        updateProgressBar(damage[0]);
+                        handler.postDelayed(this, 500);
+                    } else {
+                        childDocument.getReference().update("bossAlive", false);
+                        monsterImage.setImageResource(currBoss.getDamagedImageResId());
+                        showPopup("You have defeated me!","Great!");
+                    }
+                }
+            };
+            handler.post(damageRunnable);
+        } else{
+            Runnable damageRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    if (i[0]>0) {
+                        damage[0] +=5;
+                        i[0]--;
+                        updateProgressBar(damage[0]);
+                        handler.postDelayed(this, 500);
+                    } else {
+                        showPopup("Come Back When You Are Stronger","Okay");
+                    }
+                }
+            };
+            handler.post(damageRunnable);
+        }
     }
 }
