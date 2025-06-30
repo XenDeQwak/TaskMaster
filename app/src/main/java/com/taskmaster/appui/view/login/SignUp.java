484c02adc5e4da8e4b6912796a04bc1eaf5170f6
Centhor <vincenthizon266@gmail.com>
2025-06-30 23:13:37 +0800

    Password strength hardcoded weak on less than 8 letters. Optimized some code.

--- app/src/main/java/com/taskmaster/appui/view/login/SignUp.java
+++ app/src/main/java/com/taskmaster/appui/view/login/SignUp.java
@@ -8,9 +8,6 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,12 +20,10 @@ import com.taskmaster.appui.util.NavUtil;
 public class SignUp extends AppCompatActivity {
 
     Animation pop_out_Anim, fade_in_Anim;
-    ImageView container1, container2, container3, bg, logo, logo_shadow;
-    TextView logInTextView;
     boolean hasSpecial,hasUppercase,hasNumber;
     AppCompatButton confirmButton;
     EditText emailbox, passwordbox, usernamebox, firstnamebox, lastnamebox;
-
+    FrameLayout[] passwordStrength = new FrameLayout[3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +41,11 @@ public class SignUp extends AppCompatActivity {
         lastnamebox = findViewById(R.id.lastnameSignUpBox);
         confirmButton = findViewById(R.id.confirmButton);
 
-        FrameLayout passwordStrengthBarWeakFrame = findViewById(R.id.passwordStrengthBarWeakFrame);
-        FrameLayout passwordStrengthBarAverageFrame = findViewById(R.id.passwordStrengthBarAverageFrame);
-        FrameLayout passwordStrengthBarStrongFrame = findViewById(R.id.passwordStrengthBarStrongFrame);
+        passwordStrength = new FrameLayout[] {
+                findViewById(R.id.passwordStrengthBarWeakFrame),
+                findViewById(R.id.passwordStrengthBarAverageFrame),
+                findViewById(R.id.passwordStrengthBarStrongFrame)
+        };
 
         View[] signUpInformation = {emailbox, usernamebox, passwordbox, firstnamebox, lastnamebox};
 
@@ -57,8 +54,6 @@ public class SignUp extends AppCompatActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                confirmButton.setEnabled(s.length() >= 8);
-
                 hasUppercase = s.toString().matches(".*[A-Z].*");
                 hasNumber = s.toString().matches(".*[0-9].*");
                 hasSpecial = s.toString().matches(".*[!@#$%^&+=].*");
@@ -70,32 +65,23 @@ public class SignUp extends AppCompatActivity {
                 if(hasUppercase){checkBoxes++;}
                 if(hasNumber){checkBoxes++;}
                 if(hasSpecial){checkBoxes++;}
-                if(s.length()>=8) {checkBoxes++;}
                 confirmButton.setEnabled(checkBoxes != 0);
 
-                if (checkBoxes == 1||checkBoxes==0) {
-                    passwordStrengthBarWeakFrame.setVisibility(View.VISIBLE);
-                }else passwordStrengthBarWeakFrame.setVisibility(View.GONE);
-                if (checkBoxes == 2) {
-                    passwordStrengthBarAverageFrame.setVisibility(View.VISIBLE);
-                } else passwordStrengthBarAverageFrame.setVisibility(View.GONE);
-                if (checkBoxes >= 3) {
-                    passwordStrengthBarStrongFrame.setVisibility(View.VISIBLE);
-                } else passwordStrengthBarStrongFrame.setVisibility(View.GONE);
-
-
-
+                if(s.length()<8) {
+                    passwordStrength[0].setVisibility(View.VISIBLE);
+                }else {
+                    for (FrameLayout f : passwordStrength) f.setVisibility(View.GONE);
+                    if(checkBoxes==0) {passwordStrength[checkBoxes].setVisibility(View.VISIBLE);
+                    }else{passwordStrength[checkBoxes-1].setVisibility(View.VISIBLE);}
+                }
             }
         });
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("JIAHSIAHSIAJSIOAJSIOAJSIAJSIJSIO");
-                // Sign-up logic
-                // Yes Im also using LogInManager for SignUp because why not
-                LogInManager logInManager = new LogInManager();
-                logInManager.attemptUserSignUp(signUpInformation, SignUp.this);
-            }
+
+        confirmButton.setOnClickListener(v -> {
+            // Sign-up logic
+            // Yes Im also using LogInManager for SignUp because why not
+            LogInManager logInManager = new LogInManager();
+            logInManager.attemptUserSignUp(signUpInformation, SignUp.this);
         });
     }
 }
